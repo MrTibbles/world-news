d5c7edd96f7e182b35a3b61536fb18069e0ccc88
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-31 10:25:39 +0100

    client: Adds Home page and route, adds MainContentArea layout container component

--- src/client/app.js
+++ src/client/app.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "normalize.css";
 import "./styles/base.css";
 
-const App = () => <div>World News</div>;
+import { MainContentArea } from "./layout";
+import * as Pages from "./pages";
+
+const App = () => (
+  <Router>
+    <MainContentArea>
+      <Switch>
+        <Route component={Pages.Home} exact path="/" />
+      </Switch>
+    </MainContentArea>
+  </Router>
+);
 
 ReactDom.render(<App />, document.getElementById("app-root"));
