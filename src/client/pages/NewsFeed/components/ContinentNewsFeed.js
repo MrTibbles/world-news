f537178d986227e2b032a63de118def6aa27c160
Freddie Tibbles <freddie@tibblesscribbles.com>
2019-07-31 13:29:22 +0100

    client: Adds share buttons to NewsFeedItems

--- src/client/pages/NewsFeed/components/ContinentNewsFeed.js
+++ src/client/pages/NewsFeed/components/ContinentNewsFeed.js
@@ -1,6 +1,14 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { styled } from "linaria/react";
+import {
+  EmailShareButton,
+  EmailIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon
+} from "react-share";
 
 const List = styled.ul`
   list-style: none;
@@ -9,6 +17,17 @@ const List = styled.ul`
   li {
     padding: var(--spacing-base) 0;
     margin-bottom: var(--spacing-base);
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const ShareButtons = styled.div`
+  display: flex;
+  padding-top: var(--spacing-base);
+
+  .SocialMediaShareButton {
+    margin-right: var(--spacing-sml);
   }
 `;
 
@@ -19,7 +38,23 @@ const ContinentNewsFeed = ({ items }) => {
     <List>
       {/* REPLACE INDEX KEYS WITH IDS WHEN GQL IS IMPLEMENTED */}
       {newsStories.map((item, idx) => (
-        <li key={idx}>{item.title}</li>
+        <li key={idx}>
+          <h3>{item.title}</h3>
+          <ShareButtons>
+            <EmailShareButton
+              subject={`Hey read this article on: ${item.title}`}
+              url={window.location.href}
+            >
+              <EmailIcon round={true} size={22} />
+            </EmailShareButton>
+            <FacebookShareButton quote={item.title} url={window.location.href}>
+              <FacebookIcon round={true} size={22} />
+            </FacebookShareButton>
+            <TwitterShareButton title={item.title} url={window.location.href}>
+              <TwitterIcon round={true} size={22} />
+            </TwitterShareButton>
+          </ShareButtons>
+        </li>
       ))}
     </List>
   );
